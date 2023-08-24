38a375aaf17cfbf6d82ceef10ac27d6ed3821b4e
imasshaWe <imasha98.we@gmail.com>
2023-08-24 20:37:34 +0530

    Integrate Authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './auth/auth.guard';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
-import { AuthService } from './auth/auth.service';
+import { UserModule } from './user/user.module';
 import { UserService } from './user/user.service';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/mongodb'), AuthModule],
   controllers: [AppController],
-  providers: [
-    { provide: APP_GUARD, useClass: AuthGuard },
-    AppService,
-    AuthService,
-    UserService,
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/mongodb'),
+    AuthModule,
+    UserModule,
   ],
+  providers: [AppService, { provide: APP_GUARD, useClass: AuthGuard }],
 })
 export class AppModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,20 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './user.controller';
+import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schemas';
-import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-    JwtModule.register({
-      global: true,
-      secret: 'secret',
-      signOptions: { expiresIn: '60s' },
-    }),
   ],
-  controllers: [AuthController],
+  controllers: [UserController],
   providers: [UserService],
 })
-export class AuthModule {}
+export class UserModule {}
