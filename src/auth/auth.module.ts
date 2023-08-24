1706abe8cee307848e08bf011c18882cb2df26e7
imasshaWe <imasha98.we@gmail.com>
2023-08-24 20:06:00 +0530

    Add Authentication

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from './schemas/user.schemas';
+import { JwtModule } from '@nestjs/jwt';
+import { UserService } from 'src/user/user.service';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    JwtModule.register({
+      global: true,
+      secret: 'secret',
+      signOptions: { expiresIn: '60s' },
+    }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, UserService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,48 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { LoginDto } from './dto/login-dto';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { User } from './schemas/user.schemas';
-import { compareHashPassword } from 'src/utils/password-hash';
+import { UserService } from 'src/user/user.service';
+import { JwtService } from '@nestjs/jwt';
+
 @Injectable()
 export class AuthService {
-  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async login(loginDto: LoginDto) {
-    const user = await this.userModel.findOne({
-      username: loginDto.username,
-    });
+    const user = await this.userService.findUser(
+      loginDto.username,
+      loginDto.password,
+    );
 
-    if (!user) {
-      return 'Invalid User Name';
-    }
+    const payload = { sub: user.id, username: user.username };
 
-    const isMatch = compareHashPassword(loginDto.password, user.password);
-
-    if (!isMatch) {
-      return 'Invalid Password';
-    }
-
-    return 'Login successful';
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
-
-  // async register(loginDto: LoginDto) {
-  //   const { password, username } = loginDto;
-  //   const user = await this.userModel.findOne({
-  //     username: loginDto.username,
-  //   });
-
-  //   if (user) {
-  //     return 'User already exists';
-  //   }
-
-  //   const hashedPassword = generateHashPassword(password);
-
-  //   const hash = await this.userModel.create({
-  //     username,
-  //     password: hashedPassword,
-  //   });
-
-  //   return 'User created';
-  // }
 }
